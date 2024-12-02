b916c48390521c0c1ff0e4645e55e30464d138be
ALI <1416978277@qq.com>
2024-12-02 11:34:52 +0800

    v1.7.0.v3.lite 版本升级

--- src/main/java/cn/z/tinytoken/TinyTokenException.java
+++ src/main/java/cn/z/tinytoken/TinyTokenException.java
@@ -22,10 +22,41 @@ public class TinyTokenException extends RuntimeException {
     /**
      * TinyToken异常
      *
-     * @param message 信息
+     * @param message 详细信息
      */
     public TinyTokenException(String message) {
         super(message);
+    }
+
+    /**
+     * TinyToken异常
+     *
+     * @param message 详细信息
+     * @param cause   原因
+     */
+    public TinyTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * TinyToken异常
+     *
+     * @param cause 原因
+     */
+    public TinyTokenException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * TinyToken异常
+     *
+     * @param message            详细信息
+     * @param cause              原因
+     * @param enableSuppression  是否启用抑制
+     * @param writableStackTrace 堆栈跟踪是否为可写的
+     */
+    protected TinyTokenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
