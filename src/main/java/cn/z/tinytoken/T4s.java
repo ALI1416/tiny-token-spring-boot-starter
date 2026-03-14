845a78ab8549af33f6b1e446a2079ed456d04df6
ALI <1416978277@qq.com>
2026-03-14 09:56:27 +0800

    0314

--- src/main/java/cn/z/tinytoken/T4s.java
+++ src/main/java/cn/z/tinytoken/T4s.java
@@ -5,9 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * <h1>轻量级权限认证Spring实现</h1>
@@ -99,7 +99,7 @@ public class T4s {
      * @return 编码的字符串
      */
     public static String encode(String s) {
-        return new String(Base64Coder.encode(s.getBytes(StandardCharsets.UTF_8)));
+        return new String(Base64.getEncoder().encode(s.getBytes(StandardCharsets.UTF_8)));
     }
 
     /**
@@ -109,7 +109,7 @@ public class T4s {
      * @return 解码的字符串
      */
     public static String decode(String s) {
-        return new String(Base64Coder.decode(s), StandardCharsets.UTF_8);
+        return new String(Base64.getDecoder().decode(s), StandardCharsets.UTF_8);
     }
 
 }
